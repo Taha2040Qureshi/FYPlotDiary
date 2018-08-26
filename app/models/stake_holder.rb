906c85b7f5d804e428aba91fdee4661e2779ed1e
muhammadtaha-pikes <muhammad.taha@pikessoft.com>
2018-08-26 15:23:26 +0500

    add validation in stake holder rails

--- app/models/stake_holder.rb
+++ app/models/stake_holder.rb
@@ -7,7 +7,7 @@ class StakeHolder < ActiveRecord::Base
   accepts_nested_attributes_for :address
 
 
-  validates :name, presence: true ,length: {minimum: 3, maximum: 25}
+  validates :name, presence: true ,length: {minimum: 3, maximum: 25, message: "length must be greater than 3 and less than 25"}, format: { with: /[A-Za-z]/, message: "required only alphabet"}
   validates :f_name, presence: true ,length: {minimum: 3, maximum: 25}
   validates :cnic ,presence: true , uniqueness: {case_sensitive: false},length: { is: 15} 
 
