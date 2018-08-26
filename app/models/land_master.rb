177c1db1251d52dde640e72882507b40e520bf08
codi302 <m.rehmangames@gmail.com>
2018-08-26 18:43:45 +0500

    Update Latest

--- app/models/land_master.rb
+++ app/models/land_master.rb
@@ -8,9 +8,9 @@ class LandMaster < ActiveRecord::Base
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :installment
 
-  validates :name, presence: true,length: {minimum: 3, maximum: 25, message: "length must be greater than 3 and less than 25"}, format: { with: /[a-zA-Z]/, message: "required only alphabet"}
+  validates :name, presence: true,length: {minimum: 3, maximum: 25, message: "length must be greater than 3 and less than 25"}, format: { with:  /\A[a-zA-Z]+\z/, message: "required only alphabet"}
   validates :town_id, presence: true
-  validates :f_name, presence: true ,length:  {minimum: 3, maximum: 25, message: "length must be greater than 3 and less than 25"}, format: { with: /[^a-zA-Z]/, message: "required only alphabet"}
+  validates :f_name, presence: true ,length:  {minimum: 3, maximum: 25, message: "length must be greater than 3 and less than 25"}, format: { with:  /\A[a-zA-Z]+\z/, message: "required only alphabet"}
   validates :cnic, presence: true , uniqueness: {case_sensitive: false},length: { is: 13 , message: "must be conatin 13 number "}, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
   after_create :add_payment
 
